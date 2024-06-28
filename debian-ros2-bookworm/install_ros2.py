24d939125d1ca8aedf7cd9550adfd92cc031db77
Jon Jahren <jon.jahren@gmail.com>
2024-06-28 10:26:14 +0200

    Testing with a list to get rosdep install command to work

--- debian-ros2-bookworm/install_ros2.py
+++ debian-ros2-bookworm/install_ros2.py
@@ -96,7 +96,7 @@ def main():
                    "rti-connext-dds-6.0.1"
                    ]
 
-    run_command(["rosdep install"], + rosdep_list, shell=True)
+    run_command(["rosdep install"] + rosdep_list, shell=True)
 
     # Building ROS2
     run_command(["colcon", "build", "--symlink-install"], shell=True)
